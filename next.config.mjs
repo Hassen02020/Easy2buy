f5fe38622dc49f7917cadc13d16a650f88bdb72f
Hassen02020 <hassen02020@users.noreply.github.com>
2026-06-08 18:35:27 +0200

    fix: add pathname wildcard to remotePatterns for Unsplash images on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,14 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "plus.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
